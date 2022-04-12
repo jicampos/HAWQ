5d736e887cc9285a210a8236eb22b007b67642c7
jicampos <jivcampos8@gmail.com>
2022-04-12 14:32:54 -0500

    first export

---
+++ utils/export/utils.py
@@ -0,0 +1,16 @@
+from ..quantization_utils.quant_modules import QuantAct, QuantLinear
+
+def enable_export(module):
+    if isinstance(module, QuantAct) or isinstance(module, QuantLinear):
+        module.export_mode = True
+
+def disable_export(module):
+    if type(module) == QuantAct or type(module) == QuantLinear:
+        module.export_mode = False 
+
+def set_export_mode(module, export_mode):
+    if export_mode:
+        module.apply(enable_export)
+    else:
+        module.apply(disable_export)
+

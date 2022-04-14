614ff496d362e309f475263080b9e8f14cf86a66
jicampos <jivcampos8@gmail.com>
2022-04-14 09:37:17 -0500

    adding QuantConv2d and QuantAct

--- examples/export.py
+++ examples/export.py
@@ -1,126 +1,196 @@
 """
-Exporting the JetTagger model in FINN/ONNX format.
+Exporting HAWQ models to the ONNX format.
+
+TODO:
+    Confirm attributes of QuantAct
+    Organization  
+    High Level API Call (HAWQ -> ONNX)
+    High Level API Call (PyTorch -> HAWQ)
 """
 
+import warnings
 import os, sys
 sys.path.insert(1, os.path.abspath('.'))
 
 import torch
-from utils import q_jettagger_model
-from utils.export.utils import set_export_mode
-
-from utils.quantization_utils.function import BrevitasQuantFn
-
 from torch.onnx import register_custom_op_symbolic
-register_custom_op_symbolic("::MyReLu", BrevitasQuantFn.symbolic, 1)
+from torch._C import ListType, OptionalType
 
+from utils import q_jettagger_model, q_resnet18, q_mnist
+from utils.export.utils import set_export_mode
+from utils.quantization_utils.quant_modules import QuantAct, QuantLinear, QuantBnConv2d
+from utils.quantization_utils.quant_modules import QuantMaxPool2d, QuantAveragePool2d, QuantConv2d
+
+
+SUPPORTED_LAYERS = (QuantAct, 
+                    QuantLinear,
+                    QuantConv2d)
+
+UNSUPPORTED_LAYERS = (QuantBnConv2d, 
+                      QuantMaxPool2d, 
+                      QuantAveragePool2d)
+
+# https://github.com/Xilinx/finn-base/tree/dev/src/finn/custom_op/general
+SUPPORTED_QONNX = ['Quant', 'BipolarQuant']
+UNSUPPORTED_QONNX = ['Trunc']
+
+DOMAIN_STRING = 'hawq_onnx'
 
 # ------------------------------------------------------------
-class QuantActFunc(torch.autograd.Function):
+class QuantFunc(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x: torch.Tensor) -> torch.Tensor:
+    def forward(ctx, x, scale, zero_point, bit_width, signed, narrow_range, rounding_mode):
         return x
 
     @staticmethod
-    def symbolic(g, x):
-        return g.op("Quant", x, g.op("Constant", value_t=torch.tensor(1.23, dtype=torch.float)))
+    def symbolic(g, x, scale, zero_point, bit_width, signed, narrow_range, rounding_mode):
+        ret = g.op(f'{DOMAIN_STRING}::Quant', 
+                x, scale, zero_point, bit_width,  
+                rounding_mode_s=rounding_mode,
+                signed_i=int(signed),
+                narrow_i=int(narrow_range))
+        ret.setType(OptionalType.ofTensor())
+        return ret
 
-register_custom_op_symbolic("::QuantAct", QuantActFunc.symbolic, 1)
+
+class BinaryQuantFunc(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, scale):
+        return x
+
+    @staticmethod
+    def symbolic(g, x, scale):
+        return g.op(f'{DOMAIN_STRING}::BipolarQuant', x, scale)
+
+
+class TruncFunc(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, scale, zero_point, input_bit_width, output_bit_width, rounding_mode):
+        return x
+
+    @staticmethod
+    def symbolic(g, x, scale, zero_point, input_bit_width, output_bit_width, rounding_mode):
+        return g.op(f'{DOMAIN_STRING}::Trunc',
+                    x, scale, zero_point, input_bit_width, output_bit_width,
+                    rounding_mode_s=rounding_mode)
 
 # ------------------------------------------------------------
-class MyQuantAct(torch.nn.Module):
-    def __init__(self) -> None:
+class ExportQuantAct(torch.nn.Module):
+    def __init__(self, layer) -> None:
         super().__init__()
         self.export_mode = True
-        self.relu = torch.nn.ReLU()
 
-    def forward(self, x, act_scaling_factor=None, weight_scaling_factor=None):
-        if self.export_mode:
-            return (QuantActFunc.apply(x), act_scaling_factor)
+        self.scale = layer.act_scaling_factor.clone().detach().requires_grad_(False)
+        self.zero_point = torch.tensor(0)
+        if layer.full_precision_flag:
+            self.bit_width = torch.tensor(32)
         else:
-            return self.relu(x)
+            self.bit_width = torch.tensor(layer.activation_bit)
+        
+        self.narrow_range = 0
+        self.rounding_mode = 'ROUND'
+        if layer.quant_mode == 'symmetric':
+            self.signed = 1 
+        else:
+            self.signed = 0    
+    
+    def forward(self, x, act_scaling_factor=None, weight_scaling_factor=None):
+        if self.bit_width == 1:
+            return BinaryQuantFunc.apply(x, self.scale), act_scaling_factor 
+        return QuantFunc.apply(x, self.scale, self.zero_point, self.bit_width,
+                    self.signed, self.narrow_range, self.rounding_mode), act_scaling_factor
+
 
 # ------------------------------------------------------------
-class MyQuantLinear(torch.nn.Module):
-    def __init__(self, input_shape, out_shape) -> None:
+class ExportQuantLinear(torch.nn.Module):
+    def __init__(self, layer) -> None:
         super().__init__()
-        self.export_mode = True
-        self.fc = torch.nn.Linear(input_shape, out_shape)
+        in_features, out_features = layer.weight.shape[1], layer.weight.shape[0]
+        
+        has_bias = hasattr(layer, 'bias')
+        self.fc = torch.nn.Linear(in_features, out_features, has_bias)
+        
+        if has_bias:
+            if layer.fix_flag:
+                self.fc.weight.data = layer.weight
+                self.fc.bias.data = layer.bias
+            else:
+                self.fc.weight.data = layer.weight_integer
+                self.fc.bias.data = layer.bias_integer
+        else:
+            if layer.fix_flag:
+                self.fc.weight.data = layer.weight
+            else:
+                self.fc.weight.data = layer.weight_integer
 
     def forward(self, x, act_scaling_factor=None, weight_scaling_factor=None):
-        if self.export_mode:
-            return (self.fc(x), act_scaling_factor)
-        else:
-            return self.fc(x)
+        return self.fc(x), act_scaling_factor
 
 
-def replace_with_nn(model):
-    model = model.features
+# ------------------------------------------------------------
+class ExportQuantConv2d(torch.nn.Module):
+    def __init__(self, layer) -> None:
+        super().__init__()
+        self.conv = layer.conv
+
+    def forward(self, x, act_scaling_factor=None):
+        return self.conv(x), act_scaling_factor
+
+# ------------------------------------------------------------
+def replace_layers(model):
     for param in model.parameters():
         param.requires_grad_(False)
-    
-    from utils.quantization_utils.quant_modules import QuantAct, QuantLinear
-    for name, layer in enumerate(model):
+
+    for name in dir(model):
+        layer = getattr(model, name)
         if isinstance(layer, QuantAct):
-            print('quantact')
-            model[name] = MyQuantAct()
-        if isinstance(layer, QuantLinear):
-            print('quantlinear')
-            model[name] = MyQuantLinear(layer.weight.shape[1], layer.weight.shape[0])
+            setattr(model, name, ExportQuantAct(layer))
+        elif isinstance(layer, QuantLinear):
+            setattr(model, name, ExportQuantLinear(layer))
+        elif isinstance(layer, QuantConv2d):
+            setattr(model, name, ExportQuantConv2d(layer))
+        elif isinstance(layer, torch.nn.Sequential):
+            replace_layers(layer)
+        elif isinstance(layer, UNSUPPORTED_LAYERS):
+            raise RuntimeError(f'Unsupported layer type found {layer._get_name()}')
 
-def replace_nn_apply(model):
-    for param in model.parameters():
-        param.requires_grad_(False)
-    
-    from utils.quantization_utils.quant_modules import QuantAct, QuantLinear
-    if isinstance(model, QuantAct):
-        print('quantact')
-    if isinstance(model, QuantLinear):
-        print('quantlinear')
 
+# ------------------------------------------------------------
+def register_custom_ops():
+    register_custom_op_symbolic(f'{DOMAIN_STRING}::Quant', QuantFunc.symbolic, 1)
+    register_custom_op_symbolic(f'{DOMAIN_STRING}::BipolarQuant', BinaryQuantFunc.symbolic, 1)
+
+def export_to_qonnx(model, input, filename=None):
+    if model is None:
+        raise RuntimeError('Model cannot be None')
+    if input is None:
+        raise RuntimeError('Input cannot be None')
+
+    if filename is None:
+        from datetime import datetime
+        now = datetime.now() # current date and time
+        date_time = now.strftime('%m%d%Y%H%M%S')
+        filename = f'{model._get_name()}_{date_time}.onnx'
+
+    register_custom_ops()
+    import copy 
+    export_model = copy.deepcopy(model)
+
+    with torch.no_grad():
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            replace_layers(export_model)
+            torch.onnx.export(export_model, input, filename)
+        
+    return export_model
+
+# ------------------------------------------------------------
 if __name__ == '__main__':
-    x = torch.randn([1, 16])
-    model = q_jettagger_model()
+    x = torch.randn([1, 1, 28, 28])
+    model = q_mnist()
 
-    onnx_test = False
-    if onnx_test:
-        out = model(x) 
-        print(out) 
-        set_export_mode(model, True) 
-        torch.onnx.export(model, x, 'hawq_test.onnx') 
-        bp = 0
-    
+    export_model = export_to_qonnx(model, x)
 
-    replace_with_nn(model)
-    print(model)
-
-    setattr(model, 'quant_input', MyQuantAct())
-    setattr(model, 'quant_act1', MyQuantAct())
-    setattr(model, 'quant_act2', MyQuantAct())
-    setattr(model, 'quant_act3', MyQuantAct())
-    setattr(model, 'quant_act4', MyQuantAct())
-    print(model)
-
-    model(x)
-
-    register_custom_op_symbolic("::Quant", QuantActFunc.symbolic, 1)
-    torch.onnx.export(model, x, 'hawq_replaced_layers_v3.onnx', operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK) 
-    
-    # print(list(model.modules())[1:])
-    # # print(model(x))
-
-    
-    # keys = []
-    # for name, value in model.named_parameters():
-    #     keys.append(name)
-
-    # # with torch.no_grad():
-    # #     getattr(model, keys[0].split('.')[0]).weight.fill_(0.)  # split key to only get 'fc1'
-
-    # print(keys)
-    # # replace_with_nn(model)
-    # model.apply(replace_nn_apply)
-
-    bp = 0
-
-
+    # import torchvision.models as models
+    # resnet18 = models.resnet18()
+    # print(replace_all(q_resnet18(resnet18))

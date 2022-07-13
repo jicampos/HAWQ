9fbe4f96abda78b610612196cb750abdba247ad6
jicampos <jivcampos8@gmail.com>
2022-07-13 15:16:42 -0500

    Remove rounding and output scaling in QuantLinear & QuantAct layers

--- utils/export/export_modules.py
+++ utils/export/export_modules.py
@@ -60,7 +60,13 @@ class ExportQonnxQuantAct(nn.Module):
             self.bit_width = 32
         else:
             self.bit_width = self.layer.activation_bit
-        self.scale = self.layer.act_scaling_factor.clone().detach().requires_grad_(False)
+        self.n = 2 ** (self.bit_width - 1) - 1
+        self.clamp_min = -self.n - 1
+        self.clamp_max = self.n
+
+        self.scale = (
+            self.layer.act_scaling_factor.clone().detach().requires_grad_(False)
+        )
         self.node_inputs = (
             torch.tensor(self.scale.item(), dtype=torch.float32),  # scale
             torch.tensor(0, dtype=torch.float32),  # zero point
@@ -72,27 +78,35 @@ class ExportQonnxQuantAct(nn.Module):
             "ROUND",  # rounding mode
         )
 
-    def update_parameters(self, pre_act_scaling_factor):
+    def update_node_inputs(self, pre_act_scaling_factor):
         from ..quantization_utils.quant_utils import batch_frexp
 
         new_scale = pre_act_scaling_factor / self.scale
         m, e = batch_frexp(new_scale)
-        self.scale = (2.0**e) /  m
+        new_scale = m / (2.0**e)
+        new_scale = 1 / new_scale
         self.node_inputs = (
-            torch.tensor(self.scale.item(), dtype=torch.float32),  # scale
+            torch.tensor(new_scale.item(), dtype=torch.float32),  # scale
             torch.tensor(0, dtype=torch.float32),  # zero point
             torch.tensor(self.bit_width, dtype=torch.float32),  # bit width
         )
-    
+
     def compute_quant_act(self, z_int, pre_act_scaling_factor):
         from ..quantization_utils.quant_utils import batch_frexp
 
-        n = 2 ** (self.bit_width - 1) - 1
         new_scale = pre_act_scaling_factor / self.scale
         m, e = batch_frexp(new_scale)
-        output = z_int.type(torch.double) * m.type(torch.double)
-        output = torch.round(output / (2.0 ** e))
-        return torch.clamp(output.type(torch.float), -n-1, n)
+        m = m.type(torch.float32)
+        e = e.type(torch.float32)
+        output = z_int * m
+        output = output / (2.0**e)
+        return torch.clamp(output, self.clamp_min, self.clamp_max)
+
+    def pre_quant_scale(self, x, pre_act_scaling_factor):
+        if pre_act_scaling_factor.item() != 1:
+            # x = torch.round(x / pre_act_scaling_factor.item())
+            x = x / pre_act_scaling_factor.item()
+        return x, pre_act_scaling_factor
 
     def forward(
         self,
@@ -107,17 +121,17 @@ class ExportQonnxQuantAct(nn.Module):
             pre_act_scaling_factor = x[1]
             x = x[0]
         if pre_act_scaling_factor is None:
-            pre_act_scaling_factor = torch.tensor([1], dtype=torch.float32)
+            pre_act_scaling_factor = torch.tensor([1.0], dtype=torch.float32)
 
         if self.export_mode:
-            x = torch.round(x/pre_act_scaling_factor.item())
-            quant_node = get_quant_func(self.bit_width)
+            x, pre_act_scaling_factor = self.pre_quant_scale(x, pre_act_scaling_factor)
+            # quant_node = get_quant_func(self.bit_width)
             # x = quant_node.apply(x, *self.node_inputs, *self.node_attributes)
-            x = self.compute_quant_act(x, pre_act_scaling_factor) * self.scale 
+            x = self.compute_quant_act(x, pre_act_scaling_factor)
             model_info["quant_out_export_mode"][self.layer] = x
-            return (x, self.scale) 
+            return (x, self.scale)
         else:
-            # self.update_parameters(pre_act_scaling_factor)  
+            # self.update_node_inputs(pre_act_scaling_factor)
             x, act_scaling_factor = self.layer(
                 x,
                 pre_act_scaling_factor,
@@ -178,7 +192,7 @@ class ExportQonnxQuantLinear(nn.Module):
             x = x[0]
 
         if self.export_mode:
-            x = x / prev_act_scaling_factor.view(1, -1)  
+            # x = x / prev_act_scaling_factor.view(1, -1)
             quant_node = get_quant_func(self.weight_node_inputs[2].item())
             weights = quant_node.apply(
                 self.fc.weight.data, *self.weight_node_inputs, *self.node_attributes
@@ -192,7 +206,7 @@ class ExportQonnxQuantLinear(nn.Module):
                 )
                 x = torch.add(x, bias)
 
-            x = torch.round(x)
+            # x = torch.round(x)
             bias_scaling_factor = self.scale * prev_act_scaling_factor.clone()
             x = x * bias_scaling_factor.view(1, -1)
             model_info["dense_out_export_mode"][self.layer] = x.clone()
@@ -210,8 +224,9 @@ class ExportQonnxQuantConv2d(nn.Module):
         self.layer = layer
         self.export_mode = False
 
+        self.conv_scaling_factor = self.layer.conv_scaling_factor.detach().clone()
         self.quant_args = (
-            torch.tensor(self.layer.conv_scaling_factor, dtype=torch.float32),  # scale
+            torch.tensor(1, dtype=torch.float32),  # scale
             torch.tensor(0, dtype=torch.float32),  # zero point
             torch.tensor(self.layer.weight_bit, dtype=torch.float32),  # bit width
             int(1 if layer.quant_mode == "symmetric" else 0),  # signed
@@ -230,6 +245,8 @@ class ExportQonnxQuantConv2d(nn.Module):
         pads = self.layer.conv.padding
         if type(pads) != tuple:
             pads = (pads, pads, pads, pads)
+        elif len(pads) == 2:
+            pads = (pads[0], pads[0], pads[1], pads[1])
 
         strides = self.layer.conv.stride
         if type(strides) != tuple:
@@ -243,24 +260,67 @@ class ExportQonnxQuantConv2d(nn.Module):
 
     def forward(self, x, prev_act_scaling_factor=None):
         if type(x) is tuple:
-            pre_act_scaling_factor = x[1]
+            prev_act_scaling_factor = x[1]
             x = x[0]
         if x.ndim == 3:
             # add an extra dimension for batch size
             x = x[None]
 
+        bias_scaling_factor = self.conv_scaling_factor.view(
+            1, -1
+        ) * prev_act_scaling_factor.view(1, -1)
+        correct_output_scale = bias_scaling_factor.view(1, -1, 1, 1)
+
         if self.export_mode:
             QuantFunc = get_quant_func(self.layer.weight_bit)
             weights = QuantFunc.apply(self.layer.weight_integer, *self.quant_args)
             return (
-                ConvFunc.apply(x, weights, self.layer, *self.conv_args),
-                model_info["conv_scaling_factor"][self.layer],
+                ConvFunc.apply(x, weights, self.layer, *self.conv_args)
+                * correct_output_scale,
+                self.conv_scaling_factor,
             )
         else:
             x, conv_scaling_factor = self.layer(x, prev_act_scaling_factor)
             model_info["conv_scaling_factor"][self.layer] = conv_scaling_factor
-            print(x.shape)
             return (x, conv_scaling_factor)
+
+
+# ------------------------------------------------------------
+class ExportQonnxQuantBnConv2d(nn.Module):
+    def __init__(self, layer) -> None:
+        super().__init__()
+        self.layer = layer
+        self.export_mode = False
+
+        quant_layer = QuantConv2d()
+        quant_layer.set_param(self.layer.conv)
+        self.export_quant_conv = ExportQonnxQuantConv2d(quant_layer)
+
+        self.bn = torch.nn.BatchNorm2d(
+            self.layer.bn.num_features, self.layer.bn.eps, self.layer.bn.momentum
+        )
+        self.bn.weight = self.layer.bn.weight
+        self.bn.bias = self.layer.bn.bias
+
+        self.bn.running_mean = self.layer.bn.running_mean
+        self.bn.running_var = self.layer.bn.running_var
+
+    def __repr__(self):
+        s = f"{self.__class__.__name__}()"
+        return s
+
+    def forward(self, x, pre_act_scaling_factor=None):
+        if type(x) is tuple:
+            pre_act_scaling_factor = x[1]
+            x = x[0]
+
+        if self.export_mode:
+            x, conv_scaling_factor = self.export_quant_conv(x)
+            return (self.bn(x), conv_scaling_factor)
+        else:
+            x, convbn_scaling_factor = self.layer(x, pre_act_scaling_factor)
+            model_info["convbn_scaling_factor"][self.layer] = convbn_scaling_factor
+            return self.export_quant_conv(x, convbn_scaling_factor)
 
 
 # ------------------------------------------------------------
@@ -303,41 +363,3 @@ class ExportQonnxQuantAveragePool2d(nn.Module):
             return (x_int * correct_scaling_factor, correct_scaling_factor)
         else:
             return (self.layer(x), x_scaling_factor)
-
-
-# ------------------------------------------------------------
-class ExportQonnxQuantBnConv2d(nn.Module):
-    def __init__(self, layer) -> None:
-        super().__init__()
-        self.layer = layer
-        self.export_mode = False
-
-        quant_layer = QuantConv2d()
-        quant_layer.set_param(self.layer.conv)
-        self.export_quant_conv = ExportQonnxQuantConv2d(quant_layer)
-
-        self.bn = torch.nn.BatchNorm2d(
-            self.layer.bn.num_features, self.layer.bn.eps, self.layer.bn.momentum
-        )
-        self.bn.weight = self.layer.bn.weight
-        self.bn.bias = self.layer.bn.bias
-
-        self.bn.running_mean = self.layer.bn.running_mean
-        self.bn.running_var = self.layer.bn.running_var
-
-    def __repr__(self):
-        s = f"{self.__class__.__name__}()"
-        return s
-
-    def forward(self, x, pre_act_scaling_factor=None):
-        if type(x) is tuple:
-            pre_act_scaling_factor = x[1]
-            x = x[0]
-
-        if self.export_mode:
-            x, conv_scaling_factor = self.export_quant_conv(x)
-            return (self.bn(x), conv_scaling_factor)
-        else:
-            x, convbn_scaling_factor = self.layer(x, pre_act_scaling_factor)
-            model_info["convbn_scaling_factor"][self.layer] = convbn_scaling_factor
-            return self.export_quant_conv(x, convbn_scaling_factor)
